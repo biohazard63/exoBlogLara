1ba8b3b1282ef5ed5c77a6e3014fac9942593317
biohazard63 <quentindevaulx63@gmail.com>
2024-04-08 11:17:19 +0200

    ajout de la page about avec sa route ok

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/abouts', function () {
+    return Inertia::render('Abouts');
+})->name('abouts');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
